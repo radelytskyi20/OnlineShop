007a6300aa769ee80c3fbae259cae9c4b24edce3
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2023-09-10 15:57:38 +0300

    Added Address to ApplicationUser model

---
+++ src/OnlineShop.Library/Common/Models/Address.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Library.Common.Models
+{
+    [Table("Addresses")]
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column(TypeName = "uniqueidentifier")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(128)")]
+        public string Country { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(128)")]
+        public string City { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(32)")]
+        public string PostalCode { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(256)")]
+        public string AddressLine1 { get; set; }
+
+        [Column(TypeName = "nvarchar(256)")]
+        public string AddressLine2 { get; set; }
+    }
+}
--- src/OnlineShop.Library/UserManagementService/Models/ApplicationUser.cs
+++ src/OnlineShop.Library/UserManagementService/Models/ApplicationUser.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OnlineShop.Library.Common.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineShop.Library.UserManagementService.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Column(TypeName = "nvarchar(256)")]
+        public string FirstName { get; set; }
 
+        [Column(TypeName = "nvarchar(256)")]
+        public string LastName { get; set; }
+        public Address DefaultAddress { get; set; }
+        public Address DeliveryAddress { get; set; }
     }
 }
