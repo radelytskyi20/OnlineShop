f78d1ccdb00456884180be8151a6f6de2326afa3
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2023-10-25 20:19:33 +0300

    RolesClient and UsersClient were created

---
+++ src/OnlineShop.Library/Clients/UserManagementService/IRolesClient.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using OnlineShop.Library.UserManagementService.Responses;
+
+namespace OnlineShop.Library.Clients.UserManagementService
+{
+    public interface IRolesClient
+    {
+        Task<IdentityResult> Add(IdentityRole role);
+
+        Task<UserManagementServiceResponse<IdentityRole>> Get(string name);
+
+        Task<UserManagementServiceResponse<IEnumerable<IdentityRole>>> GetAll();
+
+        Task<IdentityResult> Remove(IdentityRole role);
+
+        Task<IdentityResult> Update(IdentityRole role);
+    }
+}
---
+++ src/OnlineShop.Library/UserManagementService/Responses/IdentityResultDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace OnlineShop.Library.UserManagementService.Responses
+{
+    public class IdentityResultDto
+    {
+        public bool Succeeded { get; set; }
+        public IEnumerable<IdentityError> Errors { get; set; }
+    }
+}
