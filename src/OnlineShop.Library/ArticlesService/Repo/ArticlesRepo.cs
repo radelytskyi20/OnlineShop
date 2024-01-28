e44f542597164a7eb7abecb6c79c73562ee98c63
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-01-28 22:10:49 +0200

    Added rest scripts for ArticlesService

--- src/OnlineShop.Library/ArticlesService/Repo/ArticlesRepo.cs
+++ src/OnlineShop.Library/ArticlesService/Repo/ArticlesRepo.cs
@@ -1,4 +1,5 @@
-﻿using OnlineShop.Library.ArticlesService.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Library.ArticlesService.Models;
 using OnlineShop.Library.Common.Repos;
 using OnlineShop.Library.Data;
 
@@ -10,5 +11,12 @@ namespace OnlineShop.Library.ArticlesService.Repo
         {
             Table = Context.Articles;
         }
+
+        public override async Task<IEnumerable<Article>> GetAllAsync() =>
+            await Table.Include(nameof(Article.PriceLists)).ToListAsync();
+
+        public override async Task<Article> GetOneAsync(Guid id) =>
+            await Task.Run(() => Table.Include(nameof(Article.PriceLists))
+                .FirstOrDefault(entity => entity.Id == id));
     }
 }
