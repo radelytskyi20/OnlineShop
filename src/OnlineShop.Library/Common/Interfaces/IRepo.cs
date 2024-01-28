c23fbba183e25dde000664c52d34b54076a5c46f
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-01-28 16:51:21 +0200

    Created BaseRepo template. Added ArticlesRepo and PriceListsRepo

---
+++ src/OnlineShop.Library/Common/Interfaces/IRepo.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShop.Library.Common.Interfaces
+{
+    public interface IRepo<T>
+    {
+        Task<Guid> AddAsync(T entity);
+        Task<IEnumerable<Guid>> AddRangeAsync(IEnumerable<T> entities);
+        Task<int> DeleteAsync(Guid id);
+        Task<int> DeleteRangeAsync(IEnumerable<Guid> ids);
+        Task<int> SaveAsync(T entity);
+        Task<T> GetOneAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
