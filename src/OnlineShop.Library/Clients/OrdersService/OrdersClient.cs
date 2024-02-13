145f550aa0b92cc89b6002a5855f3657c330b187
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-02-13 11:00:14 +0200

    Added implementation of RepoClientBase for ArticlesService and OrdersService

---
+++ src/OnlineShop.Library/Clients/OrdersService/OrdersClient.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Options;
+using OnlineShop.Library.Constants;
+using OnlineShop.Library.Options;
+using OnlineShop.Library.OrdersService.Models;
+
+namespace OnlineShop.Library.Clients.OrdersService
+{
+    public class OrdersClient : RepoClientBase<Order>
+    {
+        public OrdersClient(HttpClient client, IOptions<ServiceAdressOptions> options) : base(client, options) { }
+
+        protected override void InitializeClient(IOptions<ServiceAdressOptions> options) => HttpClient.BaseAddress = new Uri(options.Value.OrdersService);
+        protected override void SetControllerName() => ControllerName = ControllerNames.Orders;
+    }
+}
---
+++ src/OnlineShop.Library/Constants/ControllerNames.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineShop.Library.Constants
+{
+    public class ControllerNames
+    {
+        public const string Articles = "Articles";
+        public const string PriceLists = "PriceLists";
+        public const string OrderedArticles = "OrderedArticles";
+        public const string Orders = "Orders";
+    }
+}
