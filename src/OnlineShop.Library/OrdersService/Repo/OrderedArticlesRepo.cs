195d4da1c3e246d13a046bcc1f6b0f5d9cb6d0f9
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-02-13 09:33:51 +0200

    Created implementations of BaseRepo for Order and OrderedArticle

---
+++ src/OnlineShop.Library/OrdersService/Repo/OrderedArticlesRepo.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Library.ArticlesService.Models;
+using OnlineShop.Library.Common.Repos;
+using OnlineShop.Library.Data;
+
+namespace OnlineShop.Library.OrdersService.Repo
+{
+    public class OrderedArticlesRepo : BaseRepo<OrderedArticle>
+    {
+        public OrderedArticlesRepo(OrdersDbContext context) : base(context) { }
+    }
+}
---
+++ src/OnlineShop.Library/OrdersService/Repo/OrdersRepo.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Library.Common.Repos;
+using OnlineShop.Library.Data;
+using OnlineShop.Library.OrdersService.Models;
+
+namespace OnlineShop.Library.OrdersService.Repo
+{
+    public class OrdersRepo : BaseRepo<Order>
+    {
+        public OrdersRepo(OrdersDbContext context) : base(context) { }
+
+        public override async Task<IEnumerable<Order>> GetAllAsync() => await Table.Include(nameof(Order.Articles)).ToListAsync();
+        public override async Task<Order> GetOneAsync(Guid id) => await Task.Run(() => Table.Include(nameof(Order.Articles)).FirstOrDefault(entity => entity.Id == id));
+    }
+}
