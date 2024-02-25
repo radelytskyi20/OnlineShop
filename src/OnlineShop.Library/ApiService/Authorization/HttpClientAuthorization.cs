029f5a230d0136a74551237d8392667c99b5e9dc
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-02-25 15:04:53 +0200

    Created IClientAuthorization implementation for requests authorization to services

---
+++ src/OnlineShop.Library/ApiService/Authorization/HttpClientAuthorization.cs
@@ -0,0 +1,33 @@
+﻿using IdentityModel.Client;
+using Microsoft.Extensions.Options;
+using OnlineShop.Library.Clients;
+using OnlineShop.Library.Clients.IdentityServer;
+using OnlineShop.Library.Options;
+
+namespace OnlineShop.Library.ApiService.Authorization
+{
+    /// <summary>
+    /// Provides functionality for authorizing HTTP clients using an identity server
+    /// </summary>
+    public class HttpClientAuthorization : IClientAuthorization
+    {
+        private readonly IIdentityServerClient _identityClient;
+        private readonly IdentityServerApiOptions _identityServerApiOptions;
+
+        public HttpClientAuthorization(IIdentityServerClient identityClient, IOptions<IdentityServerApiOptions> options)
+        {
+            _identityClient = identityClient;
+            _identityServerApiOptions = options.Value;
+        }
+
+        public async Task Authorize(IHttpClientContainer clientContainer)
+        {
+            if (clientContainer == null)
+                return;
+
+            var token = await _identityClient.GetApiToken(_identityServerApiOptions);
+            clientContainer.HttpClient.SetBearerToken(token.AccessToken);
+        }
+    }
+
+}
---
+++ src/OnlineShop.Library/ApiService/Authorization/IClientAuthorization.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShop.Library.Clients;
+
+namespace OnlineShop.Library.ApiService.Authorization
+{
+    /// <summary>
+    /// Defines contract to authorize requests from API to particular services like UserManagementService, OrderService, etc
+    /// </summary>
+    public interface IClientAuthorization
+    {
+        /// <summary>
+        /// Authorizes the specified client container. To use client for authorization, it should implement IHttpClientContainer
+        /// </summary>
+        Task Authorize(IHttpClientContainer clientContainer);
+    }
+
+}
