1f9ab13fecb93951b0084eafe12e6a4c6df553cc
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-05-12 15:48:34 +0300

    LoginClient was created

---
+++ src/OnlineShop.Library/Clients/UserManagementService/ILoginClient.cs
@@ -0,0 +1,11 @@
+﻿using OnlineShop.Library.Options;
+using OnlineShop.Library.UserManagementService.Models;
+
+namespace OnlineShop.Library.Clients.UserManagementService
+{
+    public interface ILoginClient
+    {
+        Task<UserManagementServiceToken> GetApiTokenByClientSeceret(IdentityServerApiOptions options);
+        Task<UserManagementServiceToken> GetApiTokenByUsernameAndPassword(IdentityServerUserNamePassword options);
+    }
+}
