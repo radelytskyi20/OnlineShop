ed38964a8e441e818733d6b3fdfaac69b25c559b
Yaroslav Radelytskyi <yaroslav.radelytskyi@gmail.com>
2024-07-25 13:37:02 +0300

    Created form for updating user personal info

--- src/OnlineShop.Ui/Models/Common/Address.cs
+++ src/OnlineShop.Ui/Models/Common/Address.cs
@@ -1,11 +1,21 @@
-﻿namespace OnlineShop.Ui.Models.Common
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Ui.Models.Common
 {
     public class Address
     {
         public Guid Id { get; set; }
+
+        [Required]
         public string Country { get; set; } = string.Empty;
+
+        [Required]
         public string City { get; set; } = string.Empty;
+
+        [Required]
         public string PostalCode { get; set; } = string.Empty;
+
+        [Required]
         public string AddressLine1 { get; set; } = string.Empty;
         public string AddressLine2 { get; set; } = string.Empty;
     }
--- src/OnlineShop.Ui/Models/Security/User.cs
+++ src/OnlineShop.Ui/Models/Security/User.cs
@@ -1,4 +1,5 @@
 ﻿using OnlineShop.Ui.Models.Common;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnlineShop.Ui.Models.Security
 {
@@ -6,8 +7,18 @@ namespace OnlineShop.Ui.Models.Security
     {
         public Guid Id { get; set; }
         public string UserName { get; set; } = string.Empty;
+
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required]
         public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Phone]
+        public string Phone { get; set; } = string.Empty;
         public Address DefaultAddress { get; set; } = new();
         public Address DeliveryAddress { get; set; } = new();
     }
